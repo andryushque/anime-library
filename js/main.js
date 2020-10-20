a95dab4feca7a39ef02c1632be77b879987f13db
Andrew Ustinov <andrew.ustinov23@gmail.com>
2020-10-20 12:31:37 +0300

    Create: intro slider section

--- js/main.js
+++ js/main.js
@@ -9,4 +9,19 @@ $(document).ready(function () {
       openedSymbol: '-'
     });
   };
+
+  /*=== 02. OwlCarousel ===*/
+  // Intro section slider
+  const introSlider = $(".intro-wrapper")
+  introSlider.owlCarousel({
+    loop: true,
+    items: 1,
+    nav: true,
+    dots: true,
+    navText: ["<span><i class='fas fa-angle-left'></i></span>", "<span><i class='fas fa-angle-right'></i></span>"],
+    smartSpeed: 1200,
+    autoHeight: false,
+    autoplay: true,
+    mouseDrag: false
+  });
 });
